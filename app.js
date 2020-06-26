6da3343390de9d517a962d0e21670c0a24bade43
Justin de Klerk <66204362+JustindeKlerk@users.noreply.github.com>
2020-06-26 22:04:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 
 const todoInput = document.querySelector(".todo-input");
-const todobutton = document.querySelector(".todo-button");
+const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 
